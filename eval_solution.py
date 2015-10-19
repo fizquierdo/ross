3c1db8f8048bbfdca7f95962d5b5d9b05bed5a23
Fernando Izquierdo <fer.izquierdo@gmail.com>
2015-10-19 11:45:53 +0100

    added plotting of the error

--- eval_solution.py
+++ eval_solution.py
@@ -4,6 +4,7 @@
 Evaluate the solution
 '''
 
+import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
 
@@ -17,26 +18,33 @@ submission=submission.rename(columns = {'Sales':'Sales_predicted'})
 
 assert(len(solution) == len(submission))
 
-# calculate the score metric defined in kaggle
-# RMSPE = sqr_root( (1/n) * sum( ((y_i-y_pred)/(y_i))^2 ) )
-
 evaluation = pd.merge(submission, solution, on = ['Id'], how = 'left' )
 
-evaluation['diff'] = evaluation['Sales'] - evaluation['Sales_predicted']
+# calculate the score metric defined in kaggle
+# RMSPE = sqr_root( (1/n) * sum( ((y_i-y_pred)/(y_i))^2 ) )
+evaluation['diff']      = evaluation['Sales'] - evaluation['Sales_predicted']
 evaluation['sqr_error'] = (evaluation['diff']/evaluation['Sales'])**2
 
-def compute_rmspe(evaluation):
-	# TODO investigate how to do this with apply directely in a dataframe
-	# calculate the RMSPE
-	sum_sqr_err = 0.0
-	for sales,pred in zip(list(evaluation['Sales']),list(evaluation['Sales_predicted'])):
-		if sales == 0:
-			assert(pred==0.0) # would be nan after dividing by 0
-		else:
-			sum_sqr_err += (float(sales - pred)/float(pred))**2
-	
-	return np.sqrt(sum_sqr_err/float(len(evaluation)))
+# some values are nan due to division by 0.0
+#rmspe = np.sqrt(np.sum(evaluation['sqr_error'])/float(len(evaluation)))
+evaluation.loc[evaluation.sqr_error.isnull(), 'sqr_error' ] = 0.0
+rmspe = np.sqrt(np.mean(evaluation['sqr_error']))
 	
 print 'Distribution of the errors'
 print evaluation.describe()
-print 'RMSPE', compute_rmspe(evaluation)
+print 'RMSPE', rmspe
+
+plt.figure();
+evaluation['diff'].hist(bins=200)
+plt.title('Distribution of difference')
+plt.show()
+evaluation['sqr_error'].hist(bins=200)
+plt.title('Distribution of square error')
+plt.show()
+evaluation['sqr_error'].plot(kind='box', title='distribution of square error')
+plt.show()
+evaluation['sqr_error'].cumsum().plot(title='cumulative sum of square error')
+plt.show()
+# to see all data points (takes very long)
+#evaluation['sqr_error'].plot()
+#plt.show()
